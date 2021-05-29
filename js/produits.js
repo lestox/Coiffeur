8958738b977c29b7974aa67b8b2594611585cd06
lestox <78364535+lestox@users.noreply.github.com>
2021-05-29 13:56:11 +0200

    1

--- js/produits.js
+++ js/produits.js
@@ -1,12 +1,22 @@
-var homme = document.getElementById("homme");
+var container = document.getElementById("container")
+var btn = document.getElementById("btn");
 
-homme.addEventListener("click", function() {
+btn.addEventListener("click", function() {
     var ourRequest = new XMLHttpRequest();
     ourRequest.open('GET', 'https://lestox.github.io/Coiffeur/js/JSON.json');
     ourRequest.onload = function() {
     var ourData = JSON.parse(ourRequest.responseText);
-    console.log(ourData[0]);
+    renderHTML(ourData);
     };
     ourRequest.send();
 })
 
+function renderHTML(data) {
+    var htmlString = "";
+
+    for (i = 0; i < data.lenght; i++) {
+        htmlString += "<h2>" + data[i].name + "</h2>";
+        htmlString += "<h3>Description</h3>";
+        htmlString += "<p>" + data[i].description + "</p>";
+    }
+}
