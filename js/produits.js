e3e2b08dd5b73da735031fcc6387ba8578a2934a
lestox <78364535+lestox@users.noreply.github.com>
2021-05-29 12:29:10 +0200

    Update produits.js

--- js/produits.js
+++ js/produits.js
@@ -2,7 +2,7 @@ var services = document.getElementById("homme");
 
 services.addEventListener("click", function() {
     var ourRequest = new XMLHttpRequest();
-    ourRequest.open('GET', '../projet front/js/JSON.js');
+    ourRequest.open('GET', '');
     ourRequest.onload = function() {
     var ourData = JSON.parse(ourRequest.responseText);
     console.log(ourData[0]);
