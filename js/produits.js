c1dbea33c8a4d69d221c4a4860ee8fa07a623350
lestox <78364535+lestox@users.noreply.github.com>
2021-05-30 19:29:43 +0200

    a

--- js/produits.js
+++ js/produits.js
@@ -6,7 +6,7 @@ var btn = document.getElementById("btn");
 
 btn.addEventListener("click", function() {
     var ourRequest = new XMLHttpRequest();
-    ourRequest.open('GET', 'https://lestox.github.io/Coiffeur/js/JSON.json');
+    ourRequest.open('GET', 'https://lestox.github.io/Coiffeur/js/produits.json');
     ourRequest.onload = function() {
     var ourData = JSON.parse(ourRequest.responseText);
     renderHTML(ourData);
@@ -15,18 +15,15 @@ btn.addEventListener("click", function() {
 })
 
 function renderHTML(data) {
-    var htmlString = "";
+    htmlString0 = "";
+    htmlString1 = "";
+    htmlString2 = "";
     for (i = 0; i < data.length; i++) {
-        i = 0;
-        htmlString += "<h2>" + data[i].name + "</h2>";
-        htmlString += "<h3>Description</h3>";
-        htmlString += "<p>" + data[i].description + "</p>";
-        htmlString += "<p>" + data[i].price + "</p>";
-        product1.insertAdjacentHTML("beforeend", htmlString);
-        i++;
-
+        window['htmlString' + i] += "<h2>" + data[i].name + "</h2>" + "<h3>Description</h3>" + "<p>" + data[i].description + "</p>" + "<p>" + data[i].price + "</p>";
     }
-
+    product1.insertAdjacentHTML("beforeend", htmlString0);
+    product2.insertAdjacentHTML("beforeend", htmlString1);
+    product3.insertAdjacentHTML("beforeend", htmlString2);
     btn.classList.add("hide-me");
 
 }
